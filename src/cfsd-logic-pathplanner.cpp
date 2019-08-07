689ae30eea1a83c3fdfc461ea12e4428a367b24b
olbender <olbender@users.noreply.github.com>
2019-08-07 20:18:11 +0200

    Object collector

--- src/cfsd-logic-pathplanner.cpp
+++ src/cfsd-logic-pathplanner.cpp
@@ -37,147 +37,7 @@ struct Object {
   float x;
   float y;
   uint32_t collectedComponents;
-
-  Object(): objectId(), type(), azimuthAngle(), zenithAngle(), w(), h(), x(), 
-    y(), collectedComponents() {}
 };
-
-class Cone {
-  public:
-    Cone(uint32_t, std::pair<cluon::data::TimeStamp, std::pair<float, float>>)
-      noexcept;
-    ~Cone() noexcept;
-
-    void addPosition(std::pair<cluon::data::TimeStamp, std::pair<float, float>>)
-      noexcept;
-    void cleanPath(cluon::data::TimeStamp, float) noexcept;
-    float getAge(cluon::data::TimeStamp) noexcept;
-    std::pair<float, float> getPosition() noexcept;
-    std::pair<float, float> getPositionPredicted(cluon::data::TimeStamp) 
-      noexcept;
-    std::pair<float, float> getVelocity() noexcept;
-    uint32_t getType() noexcept;
-    void print() noexcept;
-
-  private:
-    uint32_t m_type;
-    std::vector<std::pair<cluon::data::TimeStamp, std::pair<float, float>>>
-      m_motion;
-};
-
-inline Cone::Cone(uint32_t type,
-    std::pair<cluon::data::TimeStamp, std::pair<float, float>> position)
-  noexcept:
-  m_type(type),
-  m_motion()
-{
-  m_motion.push_back(position);
-}
-
-inline Cone::~Cone() noexcept
-{
-}
-    
-inline void Cone::addPosition(
-    std::pair<cluon::data::TimeStamp, std::pair<float, float>> position)
-      noexcept
-{
-  m_motion.push_back(position);
-}
-
-inline void Cone::cleanPath(cluon::data::TimeStamp now, float duration)
-  noexcept
-{
-  if (m_motion.size() < 2) {
-    return;
-  }
-
-  std::vector<std::pair<cluon::data::TimeStamp, std::pair<float, float>>>
-    validMotion;
-  for (size_t i{m_motion.size() - 2}; i >= 0; --i) {
-    cluon::data::TimeStamp lastSighting{m_motion[i].first};
-    float age{static_cast<float>(cluon::time::toMicroseconds(now)
-         - cluon::time::toMicroseconds(lastSighting)) / 1000000.0f};
-    if (age < duration) {
-      validMotion.insert(validMotion.begin(), m_motion[i]);
-    } else {
-      break;
-    }
-  }
-
-  m_motion = validMotion;
-}
-
-inline float Cone::getAge(cluon::data::TimeStamp now) noexcept
-{
-  cluon::data::TimeStamp lastSighting{m_motion.back().first};
-  float age{static_cast<float>(cluon::time::toMicroseconds(now)
-       - cluon::time::toMicroseconds(lastSighting)) / 1000000.0f};
-  return age;
-}
-
-inline std::pair<float, float> Cone::getPosition() noexcept
-{
-  return m_motion.back().second;
-}
-
-inline std::pair<float, float> Cone::getPositionPredicted(
-    cluon::data::TimeStamp now) noexcept
-{
-  auto position = getPosition();
-  if (m_motion.size() >= 2) {
-    cluon::data::TimeStamp lastSighting{m_motion.back().first};
-    float dt{static_cast<float>(cluon::time::toMicroseconds(now)
-         - cluon::time::toMicroseconds(lastSighting)) / 1000000.0f};
-    auto velocity = getVelocity();
-    
-    std::pair<float, float> prediction{
-      position.first + velocity.first * dt,
-      position.second + velocity.second * dt
-    };
-    return prediction;
-  }
-  return position;
-}
-
-inline std::pair<float, float> Cone::getVelocity() noexcept
-{
-  if (m_motion.size() < 2) {
-    return std::pair<float, float>{0.0f, 0.0f};
-  }
-
-  cluon::data::TimeStamp t1{m_motion[m_motion.size() - 1].first};
-  cluon::data::TimeStamp t0{m_motion[m_motion.size() - 2].first};
-  float dt{static_cast<float>(cluon::time::toMicroseconds(t1) 
-        - cluon::time::toMicroseconds(t0)) / 1000000.0f};
-
-  float a1{m_motion[m_motion.size() - 1].second.first};
-  float a0{m_motion[m_motion.size() - 2].second.first};
-  float aDiff{a1 - a0};
-  
-  float z1{m_motion[m_motion.size() - 1].second.second};
-  float z0{m_motion[m_motion.size() - 2].second.second};
-  float zDiff{z1 - z0};
-
-  return std::pair<float, float>{aDiff / dt, zDiff / dt};
-}
-
-inline uint32_t Cone::getType() noexcept
-{
-  return m_type;
-}
-
-inline void Cone::print() noexcept
-{
-  std::cout << " type: " << m_type << std::endl;
-  std::cout << " motion (" << m_motion.size() << " entries)"
-    << std::endl;
-  for (auto pos : m_motion) {
-    std::cout << cluon::time::toMicroseconds(pos.first) 
-      << ": " << pos.second.first << ", " << pos.second.second 
-      << std::endl;
-  }
-}
 
 int32_t main(int32_t argc, char **argv) {
   int32_t retCode{1};
@@ -191,9 +51,6 @@ int32_t main(int32_t argc, char **argv) {
   } else {
     bool const verbose{commandlineArguments.count("verbose") != 0};
 
-    float const sameConeDistance = 25.0f;
-
-
     cluon::OD4Session od4{
       static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
 
@@ -202,9 +59,8 @@ int32_t main(int32_t argc, char **argv) {
     cluon::data::TimeStamp frameTime;
     uint32_t currentUncompleteFrameId;
     
-    std::mutex conesMutex;
-    std::vector<Cone> cones;
-    cluon::data::TimeStamp conesTime;
+    std::mutex completeFrameMutex;
+    std::vector<Object> completeFrame;
     
     auto onObjectFrameStart{[&uncompleteFrameMutex, &uncompleteFrame,
       &frameTime, &currentUncompleteFrameId](
@@ -224,8 +80,8 @@ int32_t main(int32_t argc, char **argv) {
     }};
 
     auto onObjectFrameEnd{[&uncompleteFrameMutex, &uncompleteFrame, &frameTime,
-      &currentUncompleteFrameId, &conesMutex, &cones, &conesTime, 
-      &sameConeDistance, &verbose](cluon::data::Envelope &&envelope)
+      &currentUncompleteFrameId, &completeFrameMutex, &completeFrame, 
+      &verbose](cluon::data::Envelope &&envelope)
     {
       opendlv::logic::perception::ObjectFrameEnd msg = 
         cluon::extractMessage<opendlv::logic::perception::ObjectFrameEnd>(
@@ -233,61 +89,26 @@ int32_t main(int32_t argc, char **argv) {
 
       uint32_t objectFrameId = msg.objectFrameId();
       if (objectFrameId == currentUncompleteFrameId) {
-        std::lock_guard<std::mutex> lock1(conesMutex);
+        std::lock_guard<std::mutex> lock1(completeFrameMutex);
         std::lock_guard<std::mutex> lock2(uncompleteFrameMutex);
+      
+        completeFrame = std::vector<Object>();
         
-        if (verbose) {
-          std::cout << "Merging frame (id=" << objectFrameId 
-            << ") with cone map, numer of objects=" 
-            << uncompleteFrame.size() <<  std::endl;
-        }
-
-        conesTime = frameTime;
-
-        std::vector<Cone> conesToAdd;
         for (auto obj : uncompleteFrame) {
           if (obj.second.collectedComponents != 4) {
             std::cout << "Warning: Expected 4 collected components for object "
               << "(id=" << obj.first << ") but found " 
               << obj.second.collectedComponents << " (discarded)." << std::endl;
-            continue;
+          } else {
+            completeFrame.push_back(obj.second);
           }
-
-          uint32_t type{obj.second.type};
-          float azimuth{obj.second.azimuthAngle};
-          float zenith{obj.second.zenithAngle};
-              
-          std::pair<cluon::data::TimeStamp, std::pair<float, float>> pos{
-            frameTime, {azimuth, zenith}};
-
-          bool isInList{false};
-          for (auto &cone : cones) {
-            if (cone.getType() != type) {
-              continue;
-            }
-
-            auto conePosition{cone.getPositionPredicted(frameTime)};
-            float dAzimuth{azimuth - conePosition.first};
-            float dZenith{zenith - conePosition.second};
-            double distance{sqrt(dAzimuth * dAzimuth + dZenith * dZenith)};
-
-            if (distance < sameConeDistance) {
-              isInList = true;
-              cone.addPosition(pos);
-              std::cout << "Merge cones since " << distance << " < " 
-                << sameConeDistance << std::endl;
-              break;
-            }
-          }
-          if (!isInList) {
-            Cone cone(type, pos);
-            conesToAdd.push_back(cone);
-            std::cout << "Add new cone" << std::endl;
-          }
-
         }
-        cones.insert(std::end(cones), std::begin(conesToAdd),
-            std::end(conesToAdd));
+
+        if (verbose) {
+          std::cout << "Frame collected (id=" << objectFrameId 
+            << "), numer of objects=" 
+            << completeFrame.size() <<  std::endl;
+        }
       }
         
       uncompleteFrame = std::map<uint32_t, Object>();
@@ -395,57 +216,28 @@ int32_t main(int32_t argc, char **argv) {
       }
     }};
 
-    auto onEquilibrioception{[](cluon::data::Envelope &&)
-    {
-    }};
-    
-    auto atFrequency{[&od4, &conesMutex, &cones, &conesTime, &verbose]() 
+    auto atFrequency{[&od4, &completeFrameMutex, &completeFrame, &verbose]() 
       -> bool
       {
         {
-          std::lock_guard<std::mutex> lock(conesMutex);
+          std::lock_guard<std::mutex> lock(completeFrameMutex);
           
-          float const maxAge{1.0f};
-          float const maxPathDuration{5.0f};
-
-          // Clean
-          std::vector<Cone> validCones;
-          for (auto &cone : cones) {
-            if (cone.getAge(conesTime) < maxAge) {
-              cone.cleanPath(conesTime, maxPathDuration);
-              validCones.push_back(cone);
-            }
-          }
-          cones = validCones;
-
-
-
-
-
-
           if (verbose) {
-
             int32_t width{1280};
             int32_t height{720};
             float halfWidth{width / 2.0f};
             cv::Mat outImg(height, width, CV_8UC3, cv::Scalar(0,0,0));
-            std::cout << "--- Cones" << std::endl;
-            uint32_t n{0};
-            for (auto &cone : cones) {
+            for (auto &obj : completeFrame) {
 
-              std::cout << "cone " << ++n << std::endl;
-              cone.print();
-
-              auto position = cone.getPosition();
-              int32_t x = static_cast<int32_t>(
-                  halfWidth - position.first);
-              int32_t y = static_cast<int32_t>(
-                  height - position.second);
+              int32_t i = static_cast<int32_t>(
+                  halfWidth - obj.azimuthAngle);
+              int32_t j = static_cast<int32_t>(
+                  height - obj.zenithAngle);
               int32_t w = 10;
               int32_t h = 10;
 
               cv::Scalar color(255, 255, 255);
-              switch (cone.getType()) {
+              switch (obj.type) {
                 case 0:
                   color = cv::Scalar(0, 255, 255);
                   break;
@@ -458,7 +250,7 @@ int32_t main(int32_t argc, char **argv) {
                 default:
                   break;
               }
-              cv::ellipse(outImg, cv::Point(x, y), cv::Size(w, h), 0.0, 0.0, 
+              cv::ellipse(outImg, cv::Point(i, j), cv::Size(w, h), 0.0, 0.0, 
                   360.0, color);
             }
             cv::imshow("Path planner (local data only)", outImg);
@@ -482,8 +274,6 @@ int32_t main(int32_t argc, char **argv) {
         onObjectDirection);
     od4.dataTrigger(opendlv::logic::perception::ObjectAngularBlob::ID(),
         onObjectAngularBlob);
-    od4.dataTrigger(opendlv::logic::sensation::Equilibrioception::ID(),
-        onEquilibrioception);
 
     od4.timeTrigger(std::stoi(commandlineArguments["freq"]), atFrequency);
     

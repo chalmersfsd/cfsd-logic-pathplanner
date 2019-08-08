1905236fb8bcd796cf2bd290414ccc5bdb4851bc
olbender <olbender@users.noreply.github.com>
2019-08-08 15:08:27 +0200

    First OK version

--- src/cfsd-logic-pathplanner.cpp
+++ src/cfsd-logic-pathplanner.cpp
@@ -51,6 +51,14 @@ int32_t main(int32_t argc, char **argv) {
   } else {
     bool const verbose{commandlineArguments.count("verbose") != 0};
 
+    float const carHalfWidth{500.0f};
+
+    float const maxAimPointAzimuthAngleSpeed{500.0f};
+    float const maxAimPointZenithAngleSpeed{500.0f};
+
+    int32_t const freq{std::stoi(commandlineArguments["freq"])};
+    float const dt{1.0f / freq};
+
     cluon::OD4Session od4{
       static_cast<uint16_t>(std::stoi(commandlineArguments["cid"]))};
 
@@ -61,6 +69,11 @@ int32_t main(int32_t argc, char **argv) {
     
     std::mutex completeFrameMutex;
     std::vector<Object> completeFrame;
+
+    std::mutex modeMutex;
+    uint32_t mode{2};  // 0: auto, 1: blue, 2: yellow, 3: parking
+
+    std::pair<float, float> aimPoint{0.0f, 0.0f};
     
     auto onObjectFrameStart{[&uncompleteFrameMutex, &uncompleteFrame,
       &frameTime, &currentUncompleteFrameId](
@@ -103,6 +116,14 @@ int32_t main(int32_t argc, char **argv) {
             completeFrame.push_back(obj.second);
           }
         }
+
+        /*
+        std::sort(completeFrame.begin(), completeFrame.end(),
+            [](Object const &a, Object const &b) -> bool
+            {
+              return a.zenithAngle > b.zenithAngle;
+            });
+            */
 
         if (verbose) {
           std::cout << "Frame collected (id=" << objectFrameId 
@@ -215,12 +236,237 @@ int32_t main(int32_t argc, char **argv) {
         }
       }
     }};
+    
+    auto onSwitchStateRequest{[&modeMutex, &mode, &verbose](
+        cluon::data::Envelope &&envelope)
+    {
+      if (envelope.senderStamp() == 2901) {
+        opendlv::proxy::SwitchStateRequest msg = 
+          cluon::extractMessage<opendlv::proxy::SwitchStateRequest>(
+              std::move(envelope));
 
-    auto atFrequency{[&od4, &completeFrameMutex, &completeFrame, &verbose]() 
+        std::lock_guard<std::mutex> lock(modeMutex);
+        if (mode != static_cast<uint32_t>(msg.state())) {
+          mode = msg.state();
+          if (verbose) {
+            std::cout << "Behaviour mode changed to " << mode << std::endl;
+          }
+        }
+      }
+    }};
+
+    auto atFrequency{[&od4, &completeFrameMutex, &completeFrame, &modeMutex,
+      &mode, &aimPoint, &dt, &carHalfWidth, &maxAimPointAzimuthAngleSpeed,
+      &maxAimPointZenithAngleSpeed, &verbose]() 
       -> bool
       {
         {
-          std::lock_guard<std::mutex> lock(completeFrameMutex);
+
+          auto hasObjectInside{[](std::pair<float, float> p0, 
+              std::pair<float, float> p1, std::pair<float, float> p2, 
+              std::vector<Object> const &objects) 
+            -> bool
+            {
+              float const p0x{p0.first};
+              float const p0y{p0.second};
+              float const p1x{p1.first};
+              float const p1y{p1.second};
+              float const p2x{p2.first};
+              float const p2y{p2.second};
+
+              float const areaTimesTwo{-p1y * p2x + p0y * (-p1x + p2x) 
+                  + p0x * (p1y - p2y) + p1x * p2y};
+
+              for (auto &obj : objects) {
+                float const px{obj.azimuthAngle};
+                float const py{obj.zenithAngle};
+
+                float const s{(p0y * p2x - p0x * p2y + (p2y - p0y) * px 
+                  + (p0x - p2x) * py) / areaTimesTwo};
+                float const t{(p0x * p1y - p0y * p1x + (p0y - p1y) * px 
+                  + (p1x - p0x) * py) / areaTimesTwo};
+
+                if (s > 0.0f && t > 0.0f && (1.0f - s - t) > 0.0f) {
+                  return true;
+                }
+              }
+              return false;
+            }};
+
+          auto findAimForward{[&hasObjectInside](std::pair<float, float> p0, 
+              std::pair<float, float> p1, std::vector<Object> const &objects) 
+            -> std::pair<bool, std::pair<float, float>>
+            {
+              float const angleStep{30.0};
+              float const horizonZenithAngle{360.0f};
+              float const nearAngleLimit{100.0f};
+
+              bool foundAim{false};
+              std::pair<float, float> bestValidAim{0.0f, 0.0f};
+
+              for (float j = horizonZenithAngle; j > nearAngleLimit; 
+                  j -= angleStep) {
+                for (float i = p0.first; i < p1.first; i += angleStep) {
+                  if (std::abs(bestValidAim.first) < std::abs(i)) {
+                    continue;
+                  }
+                  
+                  std::pair<float, float> p2{i, j};
+
+                  if (!hasObjectInside(p0, p1, p2, objects)) {
+                    foundAim = true;
+                    bestValidAim = p2;
+                  }
+                }
+                if (foundAim) {
+                  break;
+                }
+              }
+              return std::pair<bool, std::pair<float, float>>(
+                  foundAim, bestValidAim);
+            }};
+
+
+          auto findAimLeft{[&hasObjectInside](std::pair<float, float> p0, 
+              std::pair<float, float> p1, std::vector<Object> const &objects) 
+            -> std::pair<bool, std::pair<float, float>>
+            {
+              bool foundAim{false};
+              std::pair<float, float> bestValidAim{0.0f, 0.0f};
+
+              for (auto &obj : objects) {
+                if (obj.type != 1 || obj.azimuthAngle < 0.0f) {
+                  continue;
+                }
+                if (bestValidAim.second > obj.zenithAngle) {
+                  continue;
+                }
+
+                std::pair<float, float> p2{obj.azimuthAngle - 1.5f * obj.w,
+                  obj.zenithAngle + obj.h};
+
+                if (!hasObjectInside(p0, p1, p2, objects)) {
+                  foundAim = true;
+                  bestValidAim = p2;
+                }
+              }
+              return std::pair<bool, std::pair<float, float>>(
+                  foundAim, bestValidAim);
+            }};
+          
+          auto findAimRight{[&hasObjectInside](std::pair<float, float> p0, 
+              std::pair<float, float> p1, std::vector<Object> const &objects) 
+            -> std::pair<bool, std::pair<float, float>>
+            {
+              bool foundAim{false};
+              std::pair<float, float> bestValidAim{0.0f, 0.0f};
+
+              for (auto &obj : objects) {
+                if (obj.type != 0 || obj.azimuthAngle > 0.0f) {
+                  continue;
+                }
+                if (bestValidAim.second > obj.zenithAngle) {
+                  continue;
+                }
+
+                std::pair<float, float> p2{obj.azimuthAngle + 1.5f * obj.w,
+                  obj.zenithAngle + obj.h};
+
+                if (!hasObjectInside(p0, p1, p2, objects)) {
+                  foundAim = true;
+                  bestValidAim = p2;
+                }
+              }
+              return std::pair<bool, std::pair<float, float>>(
+                  foundAim, bestValidAim);
+            }};
+
+
+          std::lock_guard<std::mutex> lock1(completeFrameMutex);
+          std::lock_guard<std::mutex> lock2(modeMutex);
+
+          std::pair<float, float> const p0{carHalfWidth, 0.0f};
+          std::pair<float, float> const p1{-carHalfWidth, 0.0f};
+
+          uint32_t subMode{0};
+          bool foundAim{true};
+          std::pair<float, float> aim;
+          switch (mode) {
+            case 0:
+            case 3:
+              {
+                auto res = findAimForward(p0, p1, completeFrame);
+                if (res.first) {
+                  aim = res.second;
+                  if (aim.first < -carHalfWidth / 2.0f) {
+                    res = findAimLeft(p0, p1, completeFrame);
+                    if (res.first) {
+                      subMode = 1;
+                      aim = res.second;
+                    }
+                  } else if (aim.first > carHalfWidth / 2.0f) {
+                    res = findAimRight(p0, p1, completeFrame);
+                    if (res.first) {
+                      subMode = 2;
+                      aim = res.second;
+                    }
+                  }
+                } else {
+                  foundAim = false;
+                }
+                break;
+              }
+            case 1:
+              {
+                auto res = findAimLeft(p0, p1, completeFrame);
+                if (res.first) {
+                  aim = res.second;
+                } else {
+                  res = findAimRight(p0, p1, completeFrame);
+                  if (res.first) {
+                    aim = res.second;
+                  } else {
+                    foundAim = false;
+                  }
+                }
+                break;
+              }
+            case 2:
+              {
+                auto res = findAimRight(p0, p1, completeFrame);
+                if (res.first) {
+                  aim = res.second;
+                } else {
+                  res = findAimLeft(p0, p1, completeFrame);
+                  if (res.first) {
+                    aim = res.second;
+                  } else {
+                    foundAim = false;
+                  }
+                }
+                break;
+              }
+            default:
+              break;
+          } 
+
+          float aimPointAzimuthAngleSpeed{(aim.first - aimPoint.first) / dt};
+          if (aimPointAzimuthAngleSpeed < -maxAimPointAzimuthAngleSpeed) {
+            aimPointAzimuthAngleSpeed = -maxAimPointAzimuthAngleSpeed;
+          }
+          if (aimPointAzimuthAngleSpeed > maxAimPointAzimuthAngleSpeed) {
+            aimPointAzimuthAngleSpeed = maxAimPointAzimuthAngleSpeed;
+          }
+          aimPoint.first = aimPoint.first + aimPointAzimuthAngleSpeed * dt;
+
+          float aimPointZenithAngleSpeed{(aim.second - aimPoint.second) / dt};
+          if (aimPointZenithAngleSpeed < -maxAimPointZenithAngleSpeed) {
+            aimPointZenithAngleSpeed = -maxAimPointZenithAngleSpeed;
+          }
+          if (aimPointZenithAngleSpeed > maxAimPointZenithAngleSpeed) {
+            aimPointZenithAngleSpeed = maxAimPointZenithAngleSpeed;
+          }
+          aimPoint.second = aimPoint.second + aimPointZenithAngleSpeed * dt;
           
           if (verbose) {
             int32_t width{1280};
@@ -229,12 +475,13 @@ int32_t main(int32_t argc, char **argv) {
             cv::Mat outImg(height, width, CV_8UC3, cv::Scalar(0,0,0));
             for (auto &obj : completeFrame) {
 
+              int32_t w = static_cast<int32_t>(obj.w);
+              int32_t h = static_cast<int32_t>(obj.h);
               int32_t i = static_cast<int32_t>(
-                  halfWidth - obj.azimuthAngle);
+                  halfWidth - obj.azimuthAngle - obj.w / 2.0f);
               int32_t j = static_cast<int32_t>(
-                  height - obj.zenithAngle);
-              int32_t w = 10;
-              int32_t h = 10;
+                  height - obj.zenithAngle - obj.h);
+
 
               cv::Scalar color(255, 255, 255);
               switch (obj.type) {
@@ -250,11 +497,64 @@ int32_t main(int32_t argc, char **argv) {
                 default:
                   break;
               }
-              cv::ellipse(outImg, cv::Point(i, j), cv::Size(w, h), 0.0, 0.0, 
-                  360.0, color);
+              cv::rectangle(outImg, cv::Point(i, j), cv::Point(i + w, j + h), 
+                  color);
             }
+
+            if (foundAim) {
+              int32_t p0i = static_cast<int32_t>(
+                  halfWidth - p0.first);
+              int32_t p0j = static_cast<int32_t>(
+                  height - p0.second);
+              int32_t p1i = static_cast<int32_t>(
+                  halfWidth - p1.first);
+              int32_t p1j = static_cast<int32_t>(
+                  height - p1.second);
+              int32_t p2i = static_cast<int32_t>(
+                  halfWidth - aim.first);
+              int32_t p2j = static_cast<int32_t>(
+                  height - aim.second);
+              cv::line(outImg, cv::Point(p0i, p0j), cv::Point(p2i, p2j),
+                  cv::Scalar(0, 0, 255));
+              cv::line(outImg, cv::Point(p1i, p1j), cv::Point(p2i, p2j),
+                  cv::Scalar(0, 0, 255));
+            }
+              
+
+            int32_t api = static_cast<int32_t>(
+                halfWidth - aimPoint.first);
+            int32_t apj = static_cast<int32_t>(
+                height - aimPoint.second);
+            cv::ellipse(outImg, cv::Point(api, apj), cv::Size(10, 10), 0.0, 0.0, 
+                360.0, cv::Scalar(100, 50, 255));
+
+            std::string modeText = 
+              (mode == 0 && subMode == 0) ? "auto (forwards)" 
+              : (mode == 0 && subMode == 1) ? "auto (blue)" 
+              : (mode == 0 && subMode == 2) ? "auto (yellow)" 
+              : (mode == 1) ? "blue"
+              : (mode == 2) ? "yellow"
+              : (mode == 3 && subMode == 0) ? "parking (forwards)" 
+              : (mode == 3 && subMode == 1) ? "parking (blue)" 
+              : (mode == 3 && subMode == 2) ? "parking (yellow)"
+              : (mode == 3 && subMode == 2) ? "parking (stopped)"
+              : "";
+            cv::putText(outImg, "Mode: " + modeText, cv::Point(5, 20), 0, 0.5, 
+                cv::Scalar(255, 255, 255));
+
             cv::imshow("Path planner (local data only)", outImg);
-            cv::waitKey(1);
+            char key = cv::waitKey(1);
+            if (key != -1) {
+              if (key == 'a') {
+                mode = 0;
+              } else if (key == 'b') {
+                mode = 1;
+              } else if (key == 'y') {
+                mode = 2;
+              } else if (key == 'p') {
+                mode = 3;
+              }
+            }
           }
         }
 
@@ -274,8 +574,10 @@ int32_t main(int32_t argc, char **argv) {
         onObjectDirection);
     od4.dataTrigger(opendlv::logic::perception::ObjectAngularBlob::ID(),
         onObjectAngularBlob);
+    od4.dataTrigger(opendlv::proxy::SwitchStateRequest::ID(),
+        onSwitchStateRequest);
 
-    od4.timeTrigger(std::stoi(commandlineArguments["freq"]), atFrequency);
+    od4.timeTrigger(freq, atFrequency);
     
     retCode = 0;
   }
